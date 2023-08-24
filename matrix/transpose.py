e589d5772dced23fcec088d28b9b7d667a166328
gjimenez <gustavo.jimenez.crespo@gmail.com>
2023-08-24 16:34:03 -0300

    Complete transpose function

--- matrix/transpose.py
+++ matrix/transpose.py
@@ -1,2 +1,11 @@
 def transpose(matrix):
-    pass
+    filas = len(matrix)
+    columnas = len(matrix[0])
+    matrix_trans = []
+
+    for i in range(columnas):
+        aux_1 = []
+        for j in range(filas):
+            aux_1.append(matrix[j][i])
+        matrix_trans.append(aux_1)
+    return matrix_trans
