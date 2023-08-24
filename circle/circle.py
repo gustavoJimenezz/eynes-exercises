275255e4be999aa0becdf8b9e21bf739b5b1b3f8
gjimenez <gustavo.jimenez.crespo@gmail.com>
2023-08-24 14:28:36 -0300

    Create circle class and its methods

--- circle/circle.py
+++ circle/circle.py
@@ -1,4 +1,23 @@
 import math
-
+#falta validacion
 class Circle():
-    pass
+    def __init__(self, radius) -> None:
+        self.radius = radius
+
+    def get_radius(self):
+        return self.radius
+
+    def set_radius(self,radius):
+        self.radius = radius
+
+    def get_area(self):
+        return math.pi * self.radius ** 2
+
+    def get_perimeter(self):
+        return 2 * math.pi * self.radius 
+
+    def __mul__(self, n):
+        return Circle(self.radius * n)
+
+    def __str__(self):
+        return f"Radius : {self.radius}"
